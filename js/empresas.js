a1d12044b906da9307a0a72111588039e62c8592
Jhonathan Espinosa <jhonathan2@gmail.com>
2017-07-10 21:56:13 -0500

    Cargue de logo en Empresa

--- js/empresas.js
+++ js/empresas.js
@@ -13,5 +13,18 @@ $(document).ready(function()
 		$("#lblEmpresas_Creacion_Titulo").text('Crear Empresa');
 		$("#cntEmpresas_Listado").hide();
 		$("#cntEmpresas_Creacion").slideDown();	
+
+		$("#btnEmpresas_Crear_Borrar").hide();
+	});
+
+	$("#cntEmpresas_Imagen").iniciar_CargadorImagenes({idObj : 'Empresas_Crear'});
+
+	$("#btnEmpresas_Crear_Limpiar").on("click", function(evento)
+	{
+		evento.preventDefault();
+		$("#frmEmpresas_Crear")[0].reset();
+		$('#txtEmpresas_Crear_Etiqueta').val('');
+        $('#txtEmpresas_Crear_Archivo').val('');
+        $('#imgEmpresas_Crear_Preview').attr('src', '');  
 	});
 });
